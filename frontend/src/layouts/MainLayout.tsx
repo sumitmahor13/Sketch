8750497876cf48202a6b5aeea5e0e760aaadb623
Sumit Mahor <sumitmahor13@gmail.com>
2025-07-12 15:58:16 +0530

    Frontend work

---
+++ frontend/src/layouts/MainLayout.tsx
@@ -0,0 +1,23 @@
+import Navbar from "@/components/common/Navbar";
+import React from "react";
+
+interface MainLayoutProps {
+  children: React.ReactNode;
+}
+
+const MainLayout: React.FC<MainLayoutProps> = ({children}) => {
+    return (
+        <div className="w-full min-h-screen flex flex-col">
+            <Navbar/>
+
+            <main className="">
+                {children}
+            </main>
+
+            {/* <Footer/> */}
+        
+        </div>
+    )
+}
+
+export default MainLayout;
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,14 +1,16 @@
+import MainLayout from "@/layouts/MainLayout";
 import type { RootState } from "../app/store";
 import { useSelector } from "react-redux";
 
 function Home() {
-    const user = useSelector((state: RootState) => state.auth.user);
-    return(
-        <div className="">
-            <h1>Hello {user?.name}</h1>
-            <img src={user?.profileImage} alt="Profile"/>
-        </div>
-    )
+  const user = useSelector((state: RootState) => state.auth.user);
+  return (
+    <MainLayout>
+      <div className="w-full min-h-screen flex items-center justify-center ">
+        {user && <h1 className="text-gray-700 text-5xl font-bold ">Hello {user.name.split(" ")[0]} !</h1>}
+      </div>
+    </MainLayout>
+  );
 }
 
 export default Home;
